4b37943636d3dbfbfc82a9a4170512e3e1679388
C99P <32704503+C99P@users.noreply.github.com>
2017-12-13 11:54:17 +0100

    Game7
    Level 3 Complete

---
+++ Assigment/AssigementFootballGame/Assets/Scripts/MovingObstacles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MovingObstacles : MonoBehaviour {
+	public Transform start;
+	private Vector3 obs;
+	private Vector3 obs2;
+	private float timeLenght = 8f;
+
+	void Start()
+	{
+		obs = transform.position;
+		obs2 = start.position;
+	}
+
+	void Update()
+	{
+		transform.position = Vector3.Lerp(obs, obs2,
+			Mathf.SmoothStep(0f,1f,
+				Mathf.PingPong(Time.time/timeLenght, 1f)
+			) );
+	}
+}
+
